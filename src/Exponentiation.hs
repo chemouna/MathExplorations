40cd4bed7705f9198d11291e84598388d8e555b1
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-03-24 16:23:05 +0000

    Fermat primality test.

--- src/Exponentiation.hs
+++ src/Exponentiation.hs
@@ -22,6 +22,11 @@ modExp b e m res
   | otherwise = modExp ((b * b) `mod` m) (e `div` 2) m res
 
 
+fermatPT :: Integer -> Bool
+fermatPT n = all (fermatTest n) (filter (\a -> gcd n a == 1) [1..n-1])
+             where fermatTest n a = modExp a (n-1) n 1 == 1
+
+{--
 main :: IO ()
 main =
   print $
@@ -30,3 +35,4 @@ main =
     2351399303373464486466122544523690094744975233415544072992656881240319
     (10 ^ 40)
     1
+-}
